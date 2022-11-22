5682708032eba9cec44f4156f8a441387a6a4510
artemis34x <hydroxyprop@gmail.com>
2022-11-22 15:37:00 +0100

    equipe 4

--- roman_numbers/src/main/java/Roman_numbers.java
+++ roman_numbers/src/main/java/Roman_numbers.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Roman_numbers {
-    public static void Roman_numbers(int number){
+    public static String Roman_numbers(int number){
         List<Integer> values = Arrays.asList(1000,900,500,400,100,90,50,40,10,9,5,4,1);
         List<String> romanLetters = Arrays.asList("M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I");
         String romanNumber = "";
@@ -12,8 +13,13 @@ public class Roman_numbers {
                 romanNumber = romanNumber + romanLetters.get(i);
             }
         }
-        System.out.println(romanNumber);
+        //System.out.println(romanNumber);
+        return romanNumber;
+    }
 
+    public static void main(){
+        int test=15;
+        Roman_numbers()
     }
 }
 
