0e22eb2e558785fc8fa41d7ebe7df00eaf61967f
Chems Mitta <cmitta1234@gmail.com>
2022-11-22 15:23:12 +0100

    2eme groupe

---
+++ roman_numbers/src/main/java/Roman_numbers.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Roman_numbers {
+    public static void Roman_numbers(int number){
+        List<Integer> values = Arrays.asList(1000,900,500,400,100,90,50,40,10,9,5,4,1);
+        List<String> romanLetters = Arrays.asList("M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I");
+        String romanNumber = "";
+        for (int i = 0; i < values.size(); i++) {
+            while (number >= values.get(i) && number <=values.get(i)) {
+                
+            }
+
+            }
+        }
+    }
+}
