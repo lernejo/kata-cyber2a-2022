bf9b035b7773431620e7674f16a4cd3675186127
Chems Mitta <cmitta1234@gmail.com>
2022-11-22 17:16:01 +0100

    groupe 2

--- game_of_life/src/main/java/GameOfLife.java
+++ game_of_life/src/main/java/GameOfLife.java
@@ -1,6 +1,13 @@
 public class GameOfLife {
     String population;
     public GameOfLife(String initpop){
+
         population = initpop;
+        for(int i = 0; i < 4; i++){
+            for(int j =0; j<9; j++){
+
+            }
+
+        }
     }
 }
