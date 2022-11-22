692534a2f30cadfed79b034962879f19952b05cf
Loris Dos Santos Tranchant <loris.dos-santos-tranchant>
2022-11-22 15:09:08 +0100

    tkt

---
+++ roman_numbers/src/main/java/roman_numbers.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class roman_numbers {
+    public static void Roman(int number){
+        List<Integer> values = Arrays.asList(1000,900,500,400,100,90,50,40,10,9,5,4,1);
+        List<String> romanLetters = Arrays.asList("M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I");
+
+    }
+}
